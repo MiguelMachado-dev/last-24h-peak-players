a33c17b5be4793991dac61952d9129d7368b7dba
Miguel Machado <miiguellmp@gmail.com>
2024-09-21 02:54:18 -0300

    refactor(page): improve accuracy messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,11 @@ export default function Home() {
     setAccuracy(accuracy);
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGuess(parseInt(e.target.value));
+    setAccuracy(0);
+  };
+
   useEffect(() => {
     async function fetchGames() {
       setLoading(true);
@@ -56,18 +61,19 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col">
           <p>Guess the peak player count of the last 24 hours for</p>
 
-            <h2 className="text-2xl font-bold text-center text-indigo-500"
-            >{gameName}</h2>
+          <h2 className="text-2xl font-bold text-center text-indigo-500">
+            {gameName}
+          </h2>
 
-          <p
-            className="text-sm text-gray-500"
-          >Value is rounded to the nearest whole number.</p>
+          <p className="text-sm text-gray-500">
+            Value is rounded to the nearest whole number.
+          </p>
 
           <input
             type="number"
             placeholder="Your guess"
             value={guess}
-            onChange={(e) => setGuess(parseInt(e.target.value))}
+            onChange={handleChange}
             className="p-2 border border-gray-300 rounded-md w-32 text-center text-black"
           />
 
@@ -75,23 +81,37 @@ export default function Home() {
             onClick={handleSubmit}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Submit
+            Guess!
           </button>
 
           {accuracy > 0 && accuracy <= 100 && (
-            <p
-              className="text-center text-2xl text-red-500"
-            >
-              You were <strong>{accuracy}%</strong> close to the correct number!
+            <p className="text-center text-2xl">
+              The <strong>correct</strong> peak player count was{" "}
+              <strong className="text-green-500">{playerCount}</strong>. <br />
+              Your guess of {guess} was{" "}
+              <span
+                className={
+                  accuracy > 90
+                    ? "text-green-500"
+                    : accuracy > 70
+                    ? "text-yellow-500"
+                    : "text-red-500"
+                }
+              >
+                {accuracy}%
+              </span>{" "}
+              accurate.
             </p>
           )}
 
           {accuracy > 100 && (
-            <p
-              className="text-center text-2xl text-green-500"
-            >
-              You overshot the correct number by{" "}
-              <strong>{accuracy - 100}%</strong>!
+            <p className="text-center text-2xl">
+              Guess the entire kingdom was playing, huh? Your guess was a bit
+              too high! <br />
+              The correct peak player count was{" "}
+              <span className="text-green-500">{playerCount}</span>. Your guess
+              of {guess} was{" "}
+              <span className="text-red-500">{accuracy - 100}%</span> too high.
             </p>
           )}
         </div>
